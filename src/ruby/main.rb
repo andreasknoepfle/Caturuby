75adda070c115c614cc1e667b3eabcd5e52d8d66
andreasknoepfle <andreas.knoepfle@googlemail.com>
2011-06-12 12:18:00 +0200

    Packaging System added

---
+++ src/ruby/main.rb
@@ -0,0 +1,11 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+require 'java'
+
+class Main
+  def initialize
+      puts java.lang.String.new "test"
+  end
+end
+
+Main.new
